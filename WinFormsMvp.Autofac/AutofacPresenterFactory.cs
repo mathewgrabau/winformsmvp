41916e0cd969662e78a67eb6ab10690518182259
Mathew Grabau <mgrabau@mathewgrabau.ca>
2015-08-20 02:00:50 -0500

    Implemented the Autofac support for WinFormsMvp

---
+++ WinFormsMvp.Autofac/AutofacPresenterFactory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsMvp;
+using Autofac;
+using Autofac.Core;
+using Autofac.Features.ResolveAnything;
+using WinFormsMvp.Binder;
+
+namespace WinFormsMvp.Autofac
+{
+    public class AutofacPresenterFactory : IPresenterFactory
+    {
+        protected IContainer Container;
+
+        protected Dictionary<IPresenter, ILifetimeScope> PresenterScopes = new Dictionary<IPresenter, ILifetimeScope>(); 
+
+        public AutofacPresenterFactory(IContainer container)
+        {
+            Container = container;
+        }
+
+        public IPresenter Create(Type presenterType, Type viewType, IView viewInstance)
+        {
+            var scope = Container.BeginLifetimeScope();
+
+            var presenter = scope.Resolve(presenterType, new NamedParameter("view", viewInstance)) as IPresenter;
+
+            if (presenter != null)
+            {
+                PresenterScopes.Add(presenter, scope);
+            }
+
+            return presenter;
+        }
+
+        public void Release(IPresenter presenter)
+        {
+            // TODO need to figure out the scope of the objects here.
+
+            if (PresenterScopes.ContainsKey(presenter))
+            {
+                var scope = PresenterScopes[presenter];
+                PresenterScopes.Remove(presenter);
+
+                scope.Dispose();
+            }
+
+            var disposablePresenter = presenter as IDisposable;
+            if (disposablePresenter != null)
+            {
+                disposablePresenter.Dispose();
+            }
+
+        }
+    }
+}
---
+++ WinFormsMvp.PresenterFactoryUnitTests/AutofacPresenterFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Features.ResolveAnything;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinFormsMvp.Autofac;
+
+namespace WinFormsMvp.PresenterFactoryUnitTests
+{
+    [TestClass]
+    public class AutofacPresenterFactoryTests : PresenterFactoryTests
+    {
+        protected override Binder.IPresenterFactory BuildFactory()
+        {
+            var binder = new ContainerBuilder();
+            binder.RegisterSource(new AnyConcreteTypeNotAlreadyRegisteredSource());
+            return new AutofacPresenterFactory(binder.Build());
+        }
+    }
+}
